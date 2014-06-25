d46bcebef83b76830af4fb086dde6654c47bebcd
Saurabh Bhatia <saurabh.bhatia@me.com>
2014-06-25 15:46:50 +0000

    Add ordered operations to BST

--- SymbolTables/BST.java
+++ SymbolTables/BST.java
@@ -80,4 +80,117 @@ public class BST<Key extends Comparable<Key>, Value> {
         }
         return x;   // return a link for the parent's use
     }
+    
+    /**
+     * Is the BST empty?
+     * 
+     * @return true, if BST is empty; false otherwise
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+    
+    /**
+     * Return the minimum key in the BST
+     * 
+     * @return the min key in the BST
+     */
+    public Key min() {
+        Node x = root;
+        while (x.left != null) {
+            x = x.left;
+        }
+        return x.key;
+    }
+    
+    /**
+     * Return the maximum key in the BST
+     * 
+     * @return the max key in the BST
+     */
+    public Key max() {
+        Node x = root;
+        while (x.right != null) {
+            x = x.right;
+        }
+        return x.key;
+    }
+    
+    /**
+     * Compute the floor of a key
+     *
+     * @param key: key whose floor is needed
+     * 
+     * @return the largest key in the BST which is lesser than param: key
+     */
+    public Key floor(Key key) {
+        Node x = floor(root, key);  // use recursive routine for computation
+        if (x != null) {
+            return x.key;
+        }
+        return null;
+    }
+    
+    /*
+    Recursive method to calculate floor of a key
+    */
+    private Node floor(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            // this is the floor
+            return x;
+        } else if (cmp < 0) {
+            // floor has to be in left sub-tree
+            return floor(x.left, key);
+        } else {
+            // floor is current node if not in right sub-tree
+            Node tmp = floor(x.right, key);
+            if (tmp == null) {
+                return x;
+            }
+            return tmp;
+        }
+    }
+    
+    /**
+     * Compute the ceilingf of a key
+     *
+     * @param key: key whose ceiling is needed
+     * 
+     * @return the smallest key in the BST which is larger than param: key
+     */
+    public Key ceil(Key key) {
+        Node x = ceil(root, key);  // use recursive routine for computation
+        if (x != null) {
+            return x.key;
+        }
+        return null;
+    }
+    
+    /*
+    Recursive method to calculate ceiling of a key
+    */
+    private Node ceil(Node x, Key key) {
+        if (x == null) {
+            return null;
+        }
+        int cmp = key.compareTo(x.key);
+        if (cmp == 0) {
+            // this is the floor
+            return x;
+        } else if (cmp > 0) {
+            // ceiling has to be in right sub-tree
+            return ceil(x.right, key);
+        } else {
+            // ceil is current node if not in left sub-tree
+            Node tmp = ceil(x.left, key);
+            if (tmp == null) {
+                return x;
+            }
+            return tmp;
+        }
+    }
 }
